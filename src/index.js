8dbaa87f4f304f127ddbd54025046204da174162
Daniel Velez <velez.a.daniel@gmail.com>
2018-06-06 00:26:13 -0400

    added redux to the application and made tests pass using the redux store

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
+import reducers from 'reducers';
 import App from 'components/App';
+import Root from 'root'
 
 ReactDOM.render(
-  <App />,
+  <Root>
+    <App />
+  </Root>,
   document.querySelector('#root')
 );
