df762bd2e5aaa4556484b523bb84023bcb7daa4b
Daniel Velez <velez.a.daniel@gmail.com>
2018-06-05 23:12:16 -0400

    added test for comment box and submit function

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react';
 
 class CommentBox extends Component {
+  state = { comment: '' };
+
+  handleChange = (event) => {
+    this.setState({ comment: event.target.value });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    // call an action creator
+    // and save comment
+
+    this.setState({ comment: '' });
+  };
+
   render () {
     return (
-      <form>
+      <form onSubmit={this.handleSubmit}>
         <h4> type a comment </h4>
-        <textarea />
+        <textarea onChange={this.handleChange} value={this.state.comment}/>
         <div>
           <button> submit comment </button>
         </div>
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -12,11 +12,11 @@ beforeEach(() => {
 });
 
 it('shows the comment box', () => {
-expect(wrapped.find(CommentBox).lenght).toEqual(1);
+expect(wrapped.find(CommentBox).length).toEqual(1);
 
 });
 
 it('shows the comment list', () => {
-expect(wrapped.find(CommentList).lenght).toEqual(1);
+expect(wrapped.find(CommentList).length).toEqual(1);
 
 });
