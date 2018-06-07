957d29f9edecf991b783d6a28fd36bedb4eb0171
Daniel Velez <velez.a.daniel@gmail.com>
2018-06-07 14:57:30 -0400

    created higher order component for login

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import * as actions from 'actions';
+import requireAuth from 'components/requireAuth';
 
 class CommentBox extends Component {
   state = {
@@ -20,6 +21,8 @@ class CommentBox extends Component {
   };
 
 
+
+
   render () {
     return (
       <div>
@@ -36,4 +39,6 @@ class CommentBox extends Component {
   }
 };
 
-export default connect(null, actions)(CommentBox)
+
+
+export default connect(null, actions)(requireAuth(CommentBox))
